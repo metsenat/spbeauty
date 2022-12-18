cddd47f27c6fd0b7204fbf76f4ab2b5d46a19577
metsenat <metsenat@gmail.com>
2022-12-18 05:40:00 +0000

    закончил остальное )

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,15 +1,15 @@
-import PrimaryButton from "../../../../../Components/PrimaryButton";
+//import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
+        <div className='d-inline-flex flex-column gap-2 p-4 gray-border justify-content-between col-4'>
             <div className='d-inline-flex flex-column gap-3'>
+                <div className='title pb-4'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
+                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>               
             </div>
+            <div><SecondaryButton>Добавить филиал</SecondaryButton></div>
         </div>
     );
 }

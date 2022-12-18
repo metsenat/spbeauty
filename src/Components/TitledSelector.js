217125fe035aa26184e2cc790d20e59631dc842c
metsenat <metsenat@gmail.com>
2022-12-18 03:05:16 +0000

    обновил TitledTextInput и TitledSelector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 const Select = styled.select`
-    background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
+    background: url("data:image/svg+xml,<svg height='20px' width='20px' viewBox='0 0 24 24' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M4.29289 8.29289C4.68342 7.90237 5.31658 7.90237 5.70711 8.29289L12 14.5858L18.2929 8.29289C18.6834 7.90237 19.3166 7.90237 19.7071 8.29289C20.0976 8.68342 20.0976 9.31658 19.7071 9.70711L12.7071 16.7071C12.3166 17.0976 11.6834 17.0976 11.2929 16.7071L4.29289 9.70711C3.90237 9.31658 3.90237 8.68342 4.29289 8.29289Z'/></svg>") no-repeat;
     background-position: calc(100% - 0.75rem) center !important;
     -moz-appearance:none !important;
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
@@ -19,11 +19,11 @@ const Select = styled.select`
 function TitledSelector({title, options}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className='caption my-gray'>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Select className='p-2'>
+            <Select className='p-2 px-3'>
                 {options.map(optionName => {
                     return (
                         <option key={optionName}>{optionName}</option>
--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -3,9 +3,10 @@ import styled from "styled-components";
 const Input = styled.input`
     outline: none;
     border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border: 1px solid #E2E8F0;
+    border-radius: 24px;
+    background: #F8FAFC;
+    color: #94A3B8;
     &:hover {
         background-color: #D9E2EFB8;
     }
@@ -14,11 +15,11 @@ const Input = styled.input`
 function TitledTextInput({title, def}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className='caption my-gray'>{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Input className='p-2' defaultValue={def} ></Input>
+            <Input className='p-2 px-3' defaultValue={def} ></Input>
         </div>
     );
 }
